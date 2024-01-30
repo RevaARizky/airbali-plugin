2163937a6730267f999b37d893a2bec29f769a2a
RevaARizky <revaathallah86@gmail.com>
2024-01-30 17:48:00 +0800

    responsive finally fixed

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -13,15 +13,12 @@ module.exports = {
     './src/js/**/*.js',
   ],
   theme: {
-    extend: {
-
+    colors: {
+      'dark-grey': '#636569',
+      'darker-grey': '#414A50',
+      'airbali-theme': '#D75C00',
+      white: '#ffffff'
     },
-  },
-  colors: {
-    'dark-grey': '#636569',
-    'darker-grey': '#414A50',
-    'airbali-theme': '#D75C00',
-    white: '#ffffff'
   },
 }
 
