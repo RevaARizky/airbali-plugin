c1b7cf341349f67a456b6b3520ade1f90499fdd9
RevaARizky <revaathallah86@gmail.com>
2024-02-02 04:46:09 +0800

    contact custom

--- assets/dist/js/contact.js
+++ assets/dist/js/contact.js
@@ -5,11 +5,38 @@ var __webpack_exports__ = {};
   \**********************************/
 (function ($) {
   document.addEventListener("DOMContentLoaded", function () {
-    // if(!document.querySelector('.custom-contact')) {
-    //     return false;
-    // }
+    var mainel = document.querySelectorAll('.custom-select');
+    if (mainel.length) {
+      return false;
+    }
+    var matchCustom = function matchCustom(params, data) {
+      // If there are no search terms, return all of the data
+      if (jQuery.trim(params.term) === '') {
+        return data;
+      }
 
-    console.log(jQuery().select2);
+      // Do not display the item if there is no 'text' property
+      if (typeof data.text === 'undefined') {
+        return null;
+      }
+
+      // `params.term` should be the term that is used for searching
+      // `data.text` is the text that is displayed for the data object
+      if (data.text.indexOf(params.term) > -1 || data.text == 'Custom') {
+        var modifiedData = jQuery.extend({}, data, true);
+        // You can return modified objects from here
+        // This includes matching the `children` how you want in nested data sets
+        return modifiedData;
+      }
+
+      // Return `null` if the term should not be displayed
+      return null;
+    };
+    mainel.forEach(function (el) {
+      jQuery(el).select2({
+        matcher: matchCustom
+      });
+    });
   });
 })(jQuery);
 /******/ })()
--- assets/src/js/blocks/contact.js
+++ assets/src/js/blocks/contact.js
@@ -1,11 +1,40 @@
 (($) => {
 
     document.addEventListener("DOMContentLoaded", () => {
-        // if(!document.querySelector('.custom-contact')) {
-        //     return false;
-        // }
+        const mainel = document.querySelectorAll('.custom-select')
+        if(mainel.length) {
+            return false;
+        }
 
-        console.log(jQuery().select2)
+        const matchCustom = (params, data) => {
+            // If there are no search terms, return all of the data
+            if (jQuery.trim(params.term) === '') {
+                return data;
+              }
+            
+              // Do not display the item if there is no 'text' property
+              if (typeof data.text === 'undefined') {
+                return null;
+              }
+            
+              // `params.term` should be the term that is used for searching
+              // `data.text` is the text that is displayed for the data object
+              if (data.text.indexOf(params.term) > -1 || data.text == 'Custom') {
+                var modifiedData = jQuery.extend({}, data, true);
+                // You can return modified objects from here
+                // This includes matching the `children` how you want in nested data sets
+                return modifiedData;
+              }
+            
+              // Return `null` if the term should not be displayed
+              return null;
+        }
+
+        mainel.forEach(el => {
+            jQuery(el).select2({
+                matcher: matchCustom
+            })
+        })
 
     })
 
