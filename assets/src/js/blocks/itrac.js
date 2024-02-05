8a5fb04c3f83740557f8c509c66c5447451a3db5
RevaARizky <revaathallah86@gmail.com>
2024-02-05 12:24:25 +0800

    add btn slider itrac

--- assets/src/js/blocks/itrac.js
+++ assets/src/js/blocks/itrac.js
@@ -1,3 +1,8 @@
+import Swiper from "swiper"
+import gsap from "gsap"
+import SplitText from "gsap/SplitText"
+
+
 ($ => {
 
     document.addEventListener("DOMContentLoaded", () => {
@@ -8,30 +13,61 @@
             return false
         }
 
+        gsap.registerPlugin(SplitText)
+
+        var popupSlider = mainel.querySelector('.popup-target .swiper')
+        new Swiper(popupSlider, {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        })
+        popupSlider = popupSlider.swiper
         mainel.querySelectorAll('.icon-popup-trigger').forEach(el => {
             el.addEventListener('click', () => {
-                var content = JSON.parse(el.dataset.content)
-                var popup = mainel.querySelector('.popup-target')
-                popup.querySelector('.image-target').src = content.contentImage.url
-                popup.querySelector('.title-target').innerHTML = content.title
-                popup.querySelector('.content-target').innerHTML = content.contentDescription
-                popup.classList.remove('hidden')
+                mainel.querySelector('.popup-target').classList.remove('hidden')
+                popupSlider.slideTo(el.dataset.index)
+                // popup.querySelector('.image-target').src = content.contentImage.url
+                // popup.querySelector('.title-target').innerHTML = content.title
+                // popup.querySelector('.content-target').innerHTML = content.contentDescription
             })
         })
 
         const hidePopup = () => {
             var popup = mainel.querySelector('.popup-target')
             popup.classList.add('hidden')
-            popup.querySelector('.image-target').src = ''
-            popup.querySelector('.title-target').innerHTML = ''
-            popup.querySelector('.content-target').innerHTML = ''
+            // popup.querySelector('.image-target').src = ''
+            // popup.querySelector('.title-target').innerHTML = ''
+            // popup.querySelector('.content-target').innerHTML = ''
         }
+
+        mainel.querySelector('.popup-target .prev-button').addEventListener('click', e => {
+            popupSlider.slidePrev()
+        })
+        mainel.querySelector('.popup-target .next-button').addEventListener('click', e => {
+            popupSlider.slideNext()
+        })
 
         mainel.querySelector('.popup-target .overlay').addEventListener('click', e => {
             hidePopup()
         })
-        mainel.querySelector('.popup-target .close-button').addEventListener('click', e => {
-            hidePopup()
+        mainel.querySelectorAll('.popup-target .close-button').forEach(el => {
+            el.addEventListener('click', e => {
+                hidePopup()
+            })
+        })
+
+        popupSlider.on('activeIndexChange', (slider) => {
+            if(slider.activeIndex > 0) {
+                mainel.querySelector('.popup-target .prev-button').classList.remove('hidden')
+            }
+            if(slider.activeIndex < slider.slides.length) {
+                mainel.querySelector('.popup-target .next-button').classList.remove('hidden')
+            }
+            if(slider.activeIndex == 0) {
+                mainel.querySelector('.popup-target .prev-button').classList.add('hidden')
+            }
+            if(slider.activeIndex + 1 == slider.slides.length) {
+                mainel.querySelector('.popup-target .next-button').classList.add('hidden')
+            }
         })
 
     })
